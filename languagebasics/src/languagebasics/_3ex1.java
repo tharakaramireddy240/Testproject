12a0c5957ea034542786cc6448a2339a8065a426
DELL <DELL@DESKTOP-UCGGIPJ.mshome.net>
2020-06-24 15:06:55 +0530

    first commit

---
+++ languagebasics/src/languagebasics/_3ex1.java
@@ -0,0 +1,12 @@
+package languagebasics;
+
+public class _3ex1 {
+
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println("The sum of "+a+" "+"and "+b+" is "+(a+b));
+
+	}
+
+}
